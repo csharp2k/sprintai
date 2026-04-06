4bc66d76b40e008328215d2e3954c491427023b2
csharp2k <csharp2k@gmail.com>
2026-04-05 22:00:01 -0700

    mjl

--- SprintAi/SprintAi/AppShell.xaml.cs
+++ SprintAi/SprintAi/AppShell.xaml.cs
@@ -5,12 +5,8 @@
         public AppShell()
         {
             InitializeComponent();
+
             // Register routes for category pages
-            Routing.RegisterRoute("ForYouPage", typeof(Views.Categories.ForYouPage));
-            Routing.RegisterRoute("VideoPage", typeof(Views.Categories.VideoPage));
-            Routing.RegisterRoute("LivePage", typeof(Views.Categories.LivePage));
-            Routing.RegisterRoute("SeriesPage", typeof(Views.Categories.SeriesPage));
-            Routing.RegisterRoute("FashionPage", typeof(Views.Categories.FashionPage));
             Routing.RegisterRoute("about", typeof(Views.AboutPage));
         }
     }
